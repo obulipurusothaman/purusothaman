20c2ec36ab1f82d62d4bd031efe3968b33f8a8ff
obulipurusothaman <84834718+obulipurusothaman@users.noreply.github.com>
2021-06-06 18:31:37 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="Note">
+      <h1>JAVASCRIPT AND REACT.JS</h1>
+      <p>this was an amazing bootcamp
+        it's very usefull this bootcamp take over by 
+        our LOVELY Mr.shaurya sinha sir thank you so much for this 
+        BOOTCAMP SHAPEAI
+      </p>
+      <h1>PYTHON AND MACHINE LEARNING</h1>
+      <p>this was an amazing bootcamp
+        it's very usefull this bootcamp take over by 
+        our LOVELY Mr.shaurya sinha sir thank you so much for this 
+        BOOTCAMP SHAPEAI
+      </p>
+      </div>
+  );
+}
+export default Note;
